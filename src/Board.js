e5776ba2c9621c5e6936c3475ad49ed9e89c1bb9
Saurav Adhikari <sauravads123@gmail.com>
2019-04-25 07:30:42 +0545

    Build Skeleton

---
+++ src/Board.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Square from './Square';
+class Board extends React.Component{
+
+        render()
+        {
+            return (
+                <div>
+                    <h1>hi</h1>
+                <Square/>
+                <Square/>
+                <Square/>
+                <Square/>
+                <Square/>
+                <Square/>
+                <Square/>
+                <Square/>
+                <Square/>
+                </div>
+            )
+        }
+}
+export default Board;
---
+++ src/Square.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const square = (props)=>{
+    return <button></button>
+}
+export default square;
